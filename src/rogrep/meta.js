4a301ec88160023bc4ed49e6a9c94119edeb85b8
i <175501983+iluvx@users.noreply.github.com>
2026-07-17 23:29:20 +0200

    stuff

--- src/rogrep/meta.js
+++ src/rogrep/meta.js
@@ -1,14 +1,15 @@
 // ==UserScript==
 // @name        rogrep
-// @namespace   Violentmonkey Scripts
+// @namespace   https://github.com/iluvx/rogrep
 // @description Find roblox users in public servers
 // @match       *://*.roblox.com/games/*
 // @match       *://roblox.com/games/*
 // @grant       GM_addStyle
-// @version     0.0.0
+// @version     0.0.1
 // @author      iluvx
 // @require     https://cdn.jsdelivr.net/npm/@violentmonkey/dom@2
 // @require     https://cdn.jsdelivr.net/npm/@violentmonkey/ui@0.7
+// @downloadURL https://github.com/iluvx/rogrep/raw/refs/heads/main/dist/rogrep.user.js
 // ==/UserScript==
 
 // hi
